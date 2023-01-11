5bb3c92af707f96fccc2457ff696e6232602b706
Raghu Anand <raghuaanand@gmail.com>
2023-01-11 11:24:34 +0530

    initial commit

---
+++ 2. Data Types/main.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    
+    char grade = 'A';
+    string phrase = "Giraffe Academy"; 
+    int age = 50;
+    float gpa = 2.3;
+    double gpa = 2.0; // can store more decimal than float
+    bool isMale = true;
+
+    return 0;
+}
