cea373eae046a39cbcd461fb9e31877d2f41c271
Raghu Anand <raghuaanand@gmail.com>
2023-01-11 10:56:04 +0530

    Variables

---
+++ 1. Variables/main.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+
+    string characterName = "John";
+    int age;
+    age = 35;
+    cout << "There once was a man named " << characterName << endl;
+    cout << "He was "<< age << " years old" << endl;
+    characterName = "Mike";
+    cout << "He liked the name " << characterName << endl;
+    cout << "But did not like being " << age << endl;
+    return 0;
+}
