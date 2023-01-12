f229cb95eff1d1a09aea1cee8dd33e76b66bb61c
Raghu Anand <raghuaanand@gmail.com>
2023-01-12 07:47:48 +0530

    pointers

--- 20. Pointers/main.cpp
+++ 20. Pointers/main.cpp
@@ -3,10 +3,16 @@ using namespace std;
 int main()
 {
     int myAge = 19;
+    int* pAge = &myAge;  // address of the variable is strored inside the variable pAge.
     double gpa = 7.9;
+    double *pGpa = &gpa;
     string name = "Raghu";
+    string *pName = &name;
 
-    cout << &myAge; // memory address of myAge variable
+    // cout << &myAge; // memory address of myAge variable
+
+    cout << pAge << endl;
+    cout << *pName ;  // dereferencing a pointer
 
     return 0;
 }
